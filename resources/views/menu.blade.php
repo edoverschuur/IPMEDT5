54a9a1dd4beb0bac95a847ad2d28c5bbf0561c8f
tomlommerse <tomlommerse2001@gmail.com>
2022-04-28 15:39:39 +0200

    code frontend

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/master.css">
+    <title>milieubox</title>
+</head>
+<body>
+    <nav>
+        <h1>Milieubox</h1>
+        <p>box xxxxxxx</p>
+        <img class="exit" src="img/temperature.svg" alt="temperatuur logo">
+    </nav>
+    <main class="cardcontainer">
+        <section class="card">
+            <figure class="icon">
+                <img src="img/temperature.svg" alt="temperatuur logo">
+            </figure>
+            <h2>22°C</h2>
+            <p>temperatuur</p>
+        </section>
+        <section class="card">
+            <figure class="icon">
+                <img src="img/temperature.svg" alt="temperatuur logo">
+            </figure>
+            <h2>22°C</h2>
+            <p>temperatuur</p>
+        </section>
+        <section class="card">
+            <figure class="icon">
+                <img src="img/temperature.svg" alt="temperatuur logo">
+            </figure>
+            <h2>22°C</h2>
+            <p>temperatuur</p>
+        </section>
+        <section class="card">
+            <figure class="icon">
+                <img src="img/temperature.svg" alt="temperatuur logo">
+            </figure>
+            <h2>22°C</h2>
+            <p>temperatuur</p>
+        </section>
+        <section class="card">
+            <figure class="icon">
+                <img src="img/temperature.svg" alt="temperatuur logo">
+            </figure>
+            <h2>22°C</h2>
+            <p>temperatuur</p>
+        </section>
+    </main>
+    <script src="js/main.js"></script>
+</body>
+</html>
